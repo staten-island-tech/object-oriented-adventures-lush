f73e158c25aa3cc5cf638a9114036db5c6815016
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-04-16 13:57:45 -0400

    jgd

---
+++ thegame.py
@@ -0,0 +1,4 @@
+print("As the shadows fade and the hero is faced with the challenge ahead of them...")
+name = input("What is your name?")
+print("Welcome to the maze of life, " + name + ".")
+print("As you look at what you have in your bindle, you realize you came here unprepared. Let's start with a tutorial.")
