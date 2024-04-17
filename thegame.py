bd1e04144954865dc46e94907282cddd1b65db5a
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-04-17 13:58:46 -0400

    Update thegame.py

--- thegame.py
+++ thegame.py
@@ -3,8 +3,9 @@ name = input("What is your name?")
 print("Welcome to the maze of life, " + name + ".")
 print("As you look at what you have in your bindle, you realize you came here unprepared. Let's start with a tutorial.", "go to README.md for instructions")
 
-
-
+x = spend_money:
+y = sell:
+z = trade: 
 
 
 
