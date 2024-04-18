76bf7c4679db73987941a9c2026af08b2b4158ee
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-04-18 13:34:58 -0400

    ya!!!

--- monsert.py
+++ monsert.py
@@ -1,2 +1,8 @@
 class monsters():
-    
+    def __init__(self,name,hp,atk,drops,description):
+            self.name=name
+            self.hp=hp
+            self.atk=atk
+            self.drops=drops # this is what sort items they drop, be it food or weapons
+            self.description=description
+            
