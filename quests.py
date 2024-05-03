572b6a8bdf3de67bc4134ebead4f5dc67a1e2bae
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-05-03 13:56:26 -0400

    Update quests.py

--- quests.py
+++ quests.py
@@ -1,13 +1,16 @@
 import random
-print("You encountered a trader!")
-
+print("You encountered a merchant!")
 trade = input("Would you like to trade in your weapon for an upgraded weapon? Y/N: ").upper()
-if trade == 'Y':
-    trader_ans = random.randint(1,3)
-    if trader_ans == 1:
-        print('The trader accepts the offer!')
-    else:
-        print('Trader declined :(')
-        try_again = input("Try again? Y/N: ").upper()
-        if try_again == 'Y':
-            
+
+class merchant(trade):
+
+
+
+# if trade == 'Y':
+#     trader_ans = random.randint(1,3)
+#     if trader_ans == 1:
+#         print('The trader accepts the offer!')
+#     else:
+#         print('Trader declined :(')
+#         try_again = input("Try again? Y/N: ").upper()
+#         if try_again == 'Y':
