b8f8413af335f58633e89ed0846ba7b63d33c576
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-05-03 13:45:27 -0400

    Update thegame.py
     vgv

--- thegame.py
+++ thegame.py
@@ -2,22 +2,29 @@ print("As the shadows fade and the hero is faced with the challenge ahead of the
 name = input("What is your name?")
 print("Welcome to the maze of life, " + name + ".")
 print("As you look at what you have in your bindle, you realize you came here unprepared. Let's start with a tutorial.", "go to README.md for instructions")
-
-x = spend_money:
-y = sell:
-z = trade: 
-
-
-
-
-
-
-
-
-
-
 def spend_money(ekuzanpuru):
-    # igrni
-def sell(item, user):
-    qjiqji = user.money - item.cost
-def trade(item, item):
+    # import
+    q= monster
+    p = player 
+    x = spend_money
+    y = sell
+    z = trade
+    
+    def sell(item, user):
+        item= user.money - item.cost
+        
+
+
+   
+    if p == x: 
+        print("goodjob you may now move on to the next level")
+     
+
+
+
+
+
+
+
+
+
