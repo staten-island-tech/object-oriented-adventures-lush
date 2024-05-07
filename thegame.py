5866ed4f59d6d351298719153f41d0e2b424edae
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-05-07 13:58:25 -0400

    Update thegame.py

--- thegame.py
+++ thegame.py
@@ -4,14 +4,21 @@ print("Welcome to the maze of life, " + name + ".")
 print("As you look at what you have in your bindle, you realize you came here unprepared. Let's start with a tutorial.", "go to README.md for instructions")
 def spend_money(ekuzanpuru):
     # import
-    q= monster
-    p = player 
-    x = spend_money
-    y = sell
-    z = trade
-    
-    def sell(item, user):
-        item= user.money - item.cost
+   class monster():
+    def __init__(info , monster_name , power , sell , trade , xp ):
+      info.monster_name = monster_name
+      info.power = power
+      info.sell = sell 
+      info.trade = trade 
+      info.xp = xp
+      print("monstar")
+      def attack(self , character):
+         
+          print("money total")
+def add(self , character):
+            
+            def sell(item, user):
+                item= user.money - item.cost
         
 
 
