8e9cb155ce093650e2742fa7a3bd16dec6db8572
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-05-07 13:57:50 -0400

    hhh

--- thingys.py
+++ thingys.py
@@ -1,5 +1,13 @@
 import json
 import os
+class Weapons():
+    def __init__(self,name:str,atk:int,type:str,description:str) -> None:
+        self.name = name
+        self.atk = atk
+        self.type = type
+        self.description = description
+    def __str__(self):
+        return f"{self.name}, {self.atk}, {self.type}, {self.description}"
 class Items():
     def __init__(self,name:str,type:str,description:str) -> None:
         self.name = name
@@ -8,7 +16,13 @@ class Items():
     def __str__(self):
         return f"{self.name}, {self.type}, {self.description}"
     
-Small_dagger = Items("Small dagger", )
+Small_dagger = Items("Small dagger", 20, "Weapon", "The very thing you begin your journey with.")
+Big_gender_dagger = Items("Big (gender) dagger", 40, "Weapon", "An upgrade! Not that impressive, but still progress.")
+Big_boy_bat = Items("Big boy bat", 60, "Weapon", "Despite the name, it can be used by anyone.")
+Big_sword = Items("Big sword", 50, "Weapon", "Yeah, well...what were you expecting? Slightly stronger than the big gender dagger.")
+Big_boy_bat = Items("Big boy bat", 60, "Weapon", "Despite the name, it can be used by anyone.")
+
+
 
 with open("thingys.json", "r") as f:
     data = json.load(f)
