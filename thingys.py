e3a4c07837197638f9a0ed814264ca9df4f316f5
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-05-08 13:57:17 -0400

    g

--- thingys.py
+++ thingys.py
@@ -16,11 +16,13 @@ class Items():
     def __str__(self):
         return f"{self.name}, {self.type}, {self.description}"
     
-Small_dagger = Items("Small dagger", 20, "Weapon", "The very thing you begin your journey with.")
-Big_gender_dagger = Items("Big (gender) dagger", 40, "Weapon", "An upgrade! Not that impressive, but still progress.")
-Big_boy_bat = Items("Big boy bat", 60, "Weapon", "Despite the name, it can be used by anyone.")
-Big_sword = Items("Big sword", 50, "Weapon", "Yeah, well...what were you expecting? Slightly stronger than the big gender dagger.")
-Big_boy_bat = Items("Big boy bat", 60, "Weapon", "Despite the name, it can be used by anyone.")
+Small_dagger = Weapons("Small dagger", 20, "Weapon", "The very thing you begin your journey with.")
+Big_gender_dagger = Weapons("Big (gender) dagger", 40, "Weapon", "An upgrade! Not that impressive, but still progress.")
+Big_boy_bat = Weapons("Big boy bat", 60, "Weapon", "Despite the name, it can be used by anyone.")
+Big_sword = Weapons("Big sword", 50, "Weapon", "Yeah, well...what were you expecting? Slightly stronger than the big gender dagger.")
+Pixel_sword = Weapons("Pixel sword", 80, "Weapon", "Ooh, blocky!")
+Mime_sword = Weapons("Mime sword", 100, "Weapon", "Ceci n'est pas une épée (This is not a sword). (it's invisible, but does a lot of damage anyway.)")
+Ink_sword = Weapons("Ink sword", 150, "Weapon","Weird thing from a weird guy!")
 
 
 
