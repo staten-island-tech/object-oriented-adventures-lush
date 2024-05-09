6dcd7560c994faaa2b2096cd365563395b1d9e78
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-05-09 13:47:41 -0400

    b

--- thingys.py
+++ thingys.py
@@ -16,6 +16,7 @@ class Items():
     def __str__(self):
         return f"{self.name}, {self.type}, {self.description}"
     
+#WEAPONS!!!
 Small_dagger = Weapons("Small dagger", 20, "Weapon", "The very thing you begin your journey with.")
 Big_gender_dagger = Weapons("Big (gender) dagger", 40, "Weapon", "An upgrade! Not that impressive, but still progress.")
 Big_boy_bat = Weapons("Big boy bat", 60, "Weapon", "Despite the name, it can be used by anyone.")
@@ -23,6 +24,8 @@ Big_sword = Weapons("Big sword", 50, "Weapon", "Yeah, well...what were you expec
 Pixel_sword = Weapons("Pixel sword", 80, "Weapon", "Ooh, blocky!")
 Mime_sword = Weapons("Mime sword", 100, "Weapon", "Ceci n'est pas une épée (This is not a sword). (it's invisible, but does a lot of damage anyway.)")
 Ink_sword = Weapons("Ink sword", 150, "Weapon","Weird thing from a weird guy!")
+Robot_sword = Weapons("Robot sword", 200, "Weapon", "I stole this from the kits...don't tell!")
+Garden_shears = Weapons("Garden shears", 99999999, "Weapon", "Totally not a reference to one of the most depressing games every made. Can only be used once. It does a lot of damage, but it's still fragile.")
 
 
 
