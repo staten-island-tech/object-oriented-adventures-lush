fce41a781f8c2043da2278e65627c838373bb263
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-05-10 13:56:21 -0400

    Update classes_we_ll_be_using.py

--- classes_we_ll_be_using.py
+++ classes_we_ll_be_using.py
@@ -6,8 +6,24 @@ class merchant():
         self.ask_trade = ask_trade
         self.trader_ans = trader_ans
         self.experience = experience
+class Hero:
+    def __init__(self, name, health_bar, xp, level, attack, HP):
+    self.name = name
+    self.health_bar = health_bar
+    self.xp = xp
+    self.level = level
 
-class health_bar():
-    def __init__(self, health):
-        self.health = health
+    def name():
+        name = input('What is your name?: ')
+        print('Hello' + name)
+
+    def level_up(level, xp):
+        if xp == 200:
+            level = level + 1
+            HP += 50
+            attack += 50
+        
+
+        
+
 
