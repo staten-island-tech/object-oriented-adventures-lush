31a50294ba87e68c1a6a504a93b755097db5a285
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-05-13 13:33:12 -0400

    Update classes_we_ll_be_using.py

--- classes_we_ll_be_using.py
+++ classes_we_ll_be_using.py
@@ -7,4 +7,16 @@ class merchant():
         info.trader_ans = trader_ans
         info.experience = experience
 
-class health_bar
+    def trade(info, ask_trade, trader_ans):
+        ask_trade = input("Would you like to trade in your weapon for an upgraded weapon? Y/N: ").upper()
+        while ask_trade == 'Y':
+            trader_ans = random.randint(1,3)
+            if trader_ans == 1:
+                trader_ans == 'Yes'
+                experience += 10
+                print('The trader accepts the offer!')
+            else:
+                trader_ans == 'No'
+                print('Trader declined :(')
+
+        
