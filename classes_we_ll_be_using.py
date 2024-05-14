82206865a1d1d0e4099363260deab955dcf69f38
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-05-14 13:37:58 -0400

    Update classes_we_ll_be_using.py

--- classes_we_ll_be_using.py
+++ classes_we_ll_be_using.py
@@ -1,7 +1,7 @@
 import random
 print("You encountered a merchant!")
 
-class merchant():
+class Merchant():
     def __init__(info, ask_trade, trader_ans, experience):
         info.ask_trade = ask_trade
         info.trader_ans = trader_ans
@@ -18,5 +18,35 @@ class merchant():
             else:
                 trader_ans == 'No'
                 print('Trader declined :(')
-
+                break
         
+class Monster():
+    def __init__(self, name, attack_power, health):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = health
+        self.damage = 0
+
+    def generate_mosnter():
+        
+
+    def attack(self, player):
+        player.health -= self.attack_power
+        print(f"{self.name} attacks {player.name} for {self.attack_power} damage!")
+
+    def take_damage(self,player,player_attack):
+        self.health -= player_attack
+        print(f"{player.name} attacks {self.name} for {player_attack} damage!")
+
+class NPC():
+
+
+
+
+# #in encounter code
+# provoke = input("challenge monster to a battle? Y/N: ").upper()
+# if provoke == 'Y':
+# 	monster.attack
+# else:
+# 	print("challenge forfeited")
+# ####
