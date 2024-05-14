ca6e3aa588a164b25f9ad8d8501538f75535f8c6
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-05-14 13:39:35 -0400

    gttgtt

--- npcs.py
+++ npcs.py
@@ -5,12 +5,18 @@ class NPCs():
     def __init__(self, name, description) -> None:
         self.name = name
         self.description = description
+    def __str__(self):
+        return f"{self.name}, {self.description}"
 
 dogwater = NPCs("Dogwater","Resident species.")
 Angry_lost_tech_student = NPCs("Angry lost Tech student","Forces you to finish their homework.")
+Big_boys_mom = NPCs("Big boy's mom","She's mad.")
+
 with open("npcs.json", "r") as f:
     data = json.load(f)
-    ##Call classes in here
+    data.append(dogwater.__dict__)
+    data.append(Angry_lost_tech_student.__dict__)
+    data.append(Big_boys_mom.__dict__)
 
 
 new_file = "updated.json"
