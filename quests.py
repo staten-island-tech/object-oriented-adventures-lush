ba0fe989f549f51492b9d6ca6abbe5a8b40cfbdf
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-05-16 13:18:00 -0400

    Update quests.py

--- quests.py
+++ quests.py
@@ -2,7 +2,7 @@ import random
 import merchant
 print("You encountered a merchant!")
 
-def accept(info, ask_trade, trader_ans):
+def accept():
         ask_trade = input("Would you like to trade in your weapon for an upgraded weapon? Y/N: ").upper()
         while ask_trade == 'Y':
             trader_ans = random.randint(1,3)
