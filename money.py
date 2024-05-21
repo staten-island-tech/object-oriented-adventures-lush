b2e35427e1df204ac1f57aec7022f0f7acdd6455
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-05-21 13:57:21 -0400

    Create money.py

---
+++ money.py
@@ -0,0 +1,6 @@
+from classes_we_ll_be_using import merchant
+print("you can spend ")
+spend_money = input("would you like to spend money to gain health points Y/N: ").upper()
+if spend_money == "Y":
+    print("you have now gained health points ")
+    
