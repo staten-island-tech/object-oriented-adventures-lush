6cce9aacdbc5c3683be90b449eb7b30e8ef1ec32
malakk19 <158211975+malakk19@users.noreply.github.com>
2024-05-22 13:57:19 -0400

    mn m
    fgyfjhbghvhgc

--- money.py
+++ money.py
@@ -3,4 +3,7 @@ print("you can spend ")
 spend_money = input("would you like to spend money to gain health points Y/N: ").upper()
 if spend_money == "Y":
     print("you have now gained health points ")
-    
+    input("would you like to trade wit merchant to gain xp y/n?").upper()
+else: 
+    print("you can now proceed with your game ")
+    merchant(self, ask_trade, trader_ans, experience ,  health_points):
