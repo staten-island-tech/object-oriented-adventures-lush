b6e05f018a17535bb4dbcd3ab17dff895798f6a5
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-05-23 13:21:37 -0400

    mnya

--- monaluge.py
+++ monaluge.py
@@ -1,5 +1,6 @@
-print("in this game you wil be fighting the most dangurous monsters your goal is to complete the maze and defeat the monstar and attack him using weapons")
-print("throughout the game you wil have multipl enconters with the mearchent you as the hero wil be able to gain xp points through trade with the merchant")
-print ("if you answer yes to trading wit the merchant you wil automaticly be given 100 xp")
-print ("for extra instruction and more information replay game ")
+def monologue():
+    print("in this game you will be fighting the most dangerous monsters. your goal is to complete the maze and defeat the monster and attack him using weapons")
+    print("throughout the game you wil have multiple enconters with the merchant. you, as the hero, will be able to gain xp through trade with the merchant")
+    print ("if you answer yes to trading with the merchant you wil automatically be given 100 xp")
+    print ("for extra instruction and more information, replay the game")
       
